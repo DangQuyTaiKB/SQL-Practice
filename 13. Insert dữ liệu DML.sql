d22edbf04d64574e8f2537fba403ca173601c861
DangQuyTaiKB <dangquytai776617185@gmail.com>
2024-06-16 14:07:18 +0200

    day 13: INSERT, commit, rollback

---
+++ 13. Insert dữ liệu DML.sql
@@ -0,0 +1,89 @@
+/*markdown
+- Thêm dữ liệu trong bảng với câu lệnh INSERT.
+- Lưu ý giá trị NULL khi INSERT dữ liệu.
+- INSERT dữ liệu từ các bảng khác trong cơ sở dữ liệu.
+- Cách thức sử dụng COMMIT, ROLLBACK trong cơ sở dữ liệu.
+- Tự tạo một db local và thực hành các câu lệnh SQL INSERT.
+*/
+
+-- ví dụ thêm một bản ghi
+INSERT INTO table_name (column1, column2, column3, ...)
+VALUES (value1, value2, value3, ...);
+
+-- ví dụ thêm nhiều bản ghi
+INSERT INTO table_name (column1, column2, column3, ...)
+VALUES
+    (value1, value2, value3, ...),
+    (value1, value2, value3, ...),
+    (value1, value2, value3, ...),
+    ...;
+
+
+INSERT INTO department (department_id, department_name, manager_id, location_id)
+VALUES (280, 'Finance', 108, 1700);
+
+
+-- giá trị mặc định cho cột nếu không cung cấp giá trị là NULL
+INSERT INTO department (department_id, department_name, manager_id, location_id)
+VALUES (290, 'Human Resources', 203, NULL);
+
+-- Thêm giá trị được lấy từ bảng khác
+INSERT INTO department (department_id, department_name, manager_id, location_id)
+SELECT department_id, department_name, manager_id, location_id
+FROM departments
+WHERE department_id = 10;
+
+
+-- Thêm giá trị từ một bảng khác
+INSERT INTO sale_reps(id, name, salary, commission_pct)
+SELECT employee_id, last_name, salary, commission_pct
+FROM employees
+WHERE job_id LIKE '%REP%';
+
+
+/*markdown
+Sau đây là các câu lệnh, bài tập có đáp án bạn có thể làm theo để thực hành với SQL.
+*/
+
+-- Viết câu lệnh thêm các bản ghi như sau vào bảng EMPLOYEES.
+-- — thực hiện câu lệnh INSERT với từng bản ghi như trong video
+-- — hoặc có thể thực hiện INSERT cùng lúc nhiều bản ghi như sau
+
+INSERT ALL
+INTO employees VALUES (97, 'Steven', 'Gerrald', 'george@gmail.com', '511.111.4567', '17-JUN-03', 'AD_PRES', 24000, NULL, NULL, 90)
+INTO employees VALUES (98, 'Alexander', 'Arnold', 'trent@gmail.com', '512.112.4567', '03-JAN-06', 'IT_PROG', 9000, NULL, 102, 60)
+INTO employees VALUES (99, 'Daniel', 'Faviet', 'daniel@gmail.com', '513.1113.4169', '16-AUG-02', 'FI_ACCOUNT', 9000, NULL, 108, 100)
+SELECT 1 FROM DUAL;
+
+-- Viết câu lệnh thêm các bản ghi như sau vào bảng JOB_HISTORY từ các nhân mới thêm trước đó trong bảng EMPLOYEES. 
+-- Sao cho cột START_DATE bằng cột HIRE_DATE trong bảng, cột END_DATE là 10 tháng kể từ cột START_DATE (gợi ý dùng hàm ADD_MONTHS).
+
+INSERT INTO job_history(employee_id, start_date, end_date, job_id, department_id)
+SELECT employee_id, hire_date, ADD_MONTHS(hire_date, 10), job_id, department_id
+FROM employees
+WHERE employee_id IN (97, 98, 99);
+
+INSERT INTO job_history
+SELECT employee_id,
+hire_date, ADD_MONTHS(hire_date, 10),
+job_id, department_id FROM employees
+WHERE employee_idINn (97, 98, 99);
+
+-- Viết câu lệnh thêm bản ghi vào bảng JOB_HISTORY thông tin các nhân viên có trong bảng EMPLOYEES mà không có trong bảng JOB_HISTORY.
+-- Giá trị trong cột END_DATE là 18 tháng kể từ START_DATE (HIRE_DATE trong bảng EMPLOYEES).
+
+INSERT INTO job_history(employee_id, start_date, end_date, job_id, department_id)
+SELECT employee_id, hire_date, ADD_MONTHS(hire_date, 18), job_id, department_id
+FROM employees
+WHERE employee_id NOT IN (SELECT employee_id FROM job_history);
+
+INSERT INTO job_history
+SELECT
+employee_id, hire_date AS start_date,
+ADD_MONTHS(hire_date, 18) AS end_date,
+job_id, department_id
+FROM employees
+WHERE department_id IS NOT NULL
+AND employee_id NOT IN (
+SELECT employee_id FROM job_history
+);
